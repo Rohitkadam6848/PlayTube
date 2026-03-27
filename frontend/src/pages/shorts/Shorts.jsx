c36f9a760736d18fda4a1db570cd869e01173975
Rohit Kadam <rohitkadam1230@gmail.com>
2026-03-27 19:13:11 +0530

    imporove short page

--- frontend/src/pages/shorts/Shorts.jsx
+++ frontend/src/pages/shorts/Shorts.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  FaPlay,
+  FaPause,
+  FaComment,
+  FaDownload,
+  FaBookmark,
+  FaArrowDown,
+} from "react-icons/fa";
 
 function Shorts() {
   const { allShortsData } = useSelector((state) => state.content);
   const [shortList, setShortList] = useState([]);
   const shortRef = useRef([]);
+  const [playIndex, setPlayIndex] = useState(null);
 
   useEffect(() => {
     if (!allShortsData || allShortsData.length === 0) {
@@ -38,6 +47,19 @@ function Shorts() {
 
     return () => observer.disconnect();
   }, [shortList]);
+
+  const togglePlay = (idx) => {
+    const video = shortRef.current[idx];
+    if (video) {
+      if (video.paused) {
+        video.play();
+        setPlayIndex(null);
+      } else {
+        video.pause();
+        setPlayIndex(idx);
+      }
+    }
+  };
   return (
     <div className="h-screen w-full overflow-y-scroll snap-y snap-mandatory ">
       {shortList?.map((short, idx) => (
@@ -45,7 +67,10 @@ function Shorts() {
           className="min-h-screen w-full flex md:items-center items-start justify-center snap-start relative pt-10 md:pt-0"
           key={short?._id}
         >
-          <div className="relative w-105 md:[350px] aspect-9/16 bg-black rounded-2xl overflow-hidden shadow-xl border border-gray-700 cursor-pointer ">
+          <div
+            className="relative  w-105 md:w-87.5 aspect-9/16 bg-black rounded-2xl overflow-hidden shadow-xl border border-gray-700 cursor-pointer "
+            onClick={() => togglePlay(idx)}
+          >
             <video
               ref={(el) => (shortRef.current[idx] = el)}
               data-index={idx}
@@ -54,6 +79,21 @@ function Shorts() {
               loop
               playsInline
             />
+            {playIndex === idx && (
+              <div className="absolute top-3 right-3 bg-black/60 rounded-full p-2">
+                <FaPlay className="text-white text-lg" />
+              </div>
+            )}
+
+            {playIndex !== idx && (
+              <div className="absolute top-3 right-3 bg-black/60 rounded-full p-2">
+                <FaPause className="text-white text-lg" />
+              </div>
+            )}
+
+            <div className="absolute bottom-0 left-0 right-0 p-4 bg-liner-to-t from-black/80 via-black/40 to-transparent text-white space-y-2">
+              <div></div>
+            </div>
           </div>
         </div>
       ))}
